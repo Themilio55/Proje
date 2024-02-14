fe217bee5ac91ad0a90bd170ba0d36a72fc44048
Themilio55 <156218653+Themilio55@users.noreply.github.com>
2024-02-14 21:40:15 +0400

    Add files via upload

---
+++ python proje.py
@@ -0,0 +1,13 @@
+input_list = [[1, 'a', ['cat'], 2], [[[3]], 'dog'], 4, 5]
+flatten_list = []
+
+def flatten(l):
+    for i in l:
+        if isinstance(i, list):
+            flatten(i)
+        else:
+            flatten_list.append(i)
+    return flatten_list
+
+
+print(flatten(input_list))
